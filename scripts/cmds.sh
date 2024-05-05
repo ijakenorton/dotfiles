c5109c45bcf95ac4934734aed393dc7d5b1931dc
Jake Norton <jake.norton88@gmail.com>
2024-05-06 11:01:58 +1200

    No longer tracking outside modules, easier to install

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -8,7 +8,7 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt -f install
 sh nvim_install.sh
 sh nerd_fonts.sh
-sudo apt-get install tmux xclip zsh stow alacritty -y
+sudo apt-get install tmux xclip zoxide fzf zsh stow alacritty -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
