22d00dd1ef66d69795fc532966b50d722bf3f0b5
Jake Norton <jake.norton88@gmail.com>
2024-11-28 12:02:55 +1300

    Added fish to dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    abbr --add fconfig nvim ~/.config/fish/config.fish
+    abbr --add nvc nvim ~/.config/nvim/
+    abbr --add zconfig nvim ~/.config/zellij/
+    abbr --add breakout zellij --layout ~/.config/zellij/config/layout/breakout.kdl
+    abbr --add physics zellij --layout ~/.config/zellij/config/layout/physics.kdl
+    abbr --add research zellij --layout ~/.config/zellij/config/layout/research.kdl
+    abbr --add paper zellij --layout ~/.config/zellij/config/layout/paper.kdl
+    # Commands to run in interactive sessions can go here
+end
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/jaken/miniconda3/bin/conda
+    eval /home/jaken/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/jaken/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/jaken/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/jaken/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
