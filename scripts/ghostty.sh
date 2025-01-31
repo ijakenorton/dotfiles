0be00eddd0a1130ea1009b1343b8d1e9cc5e2ee6
Jake Norton <jake.norton88@gmail.com>
2025-01-31 17:29:34 +1300

    Added -Dapp-runtime=glfw flag

--- scripts/ghostty.sh
+++ scripts/ghostty.sh
@@ -3,8 +3,8 @@ git clone https://github.com/ghostty-org/ghostty
 cd ghostty
 mise install zig
 mise use zig
-zig build -p $HOME/.local -Doptimize=ReleaseFast
+zig build -Doptimize=ReleaseFast -Dapp-runtime=glfw
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
-gsettings set org.gnome.desktop.default-applications.terminal exec /home/jaken/.local/bin/ghostty
+gsettings set org.gnome.desktop.default-applications.terminal exec $(which ghostty)
 cd ..
 rm -rf ./ghostty
